94a5da3da5e4c0d49e4cde4527279d9f94f1539b
Andrew Premdas <apremdas@yahoo.co.uk>
2011-11-24 16:46:45 +0000

    Ran 'rails new . -T -O'

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+R3MongoDevise::Application.config.secret_token = 'd084e84994fa66ff897912f4d8ada59b16369cd781887f2289f6b7395b0c26ab501c723116d2dadcf8b10fd98235d191ff66fa4b3507ba65aff4ba4b8bd03e23'
