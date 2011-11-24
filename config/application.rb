f0f8ec767d7f7f442dadef19b3340ba172ce5c09
Andrew Premdas <apremdas@yahoo.co.uk>
2011-11-24 17:07:13 +0000

    Suppress specs for views and helpers

--- config/application.rb
+++ config/application.rb
@@ -6,7 +6,6 @@ require "action_controller/railtie"
 require "action_mailer/railtie"
 require "active_resource/railtie"
 require "sprockets/railtie"
-# require "rails/test_unit/railtie"
 
 if defined?(Bundler)
   # If you precompile assets before deploying to production, use this line
@@ -50,5 +49,13 @@ module R3MongoDevise
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+
+    config.generators do |g|
+      g.view_specs false
+      g.helper_specs false
+    end
+
+
+
   end
 end
