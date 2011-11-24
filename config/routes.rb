bcb28732f5fe8065b0ccaeb4e34d7525e331724e
Andrew Premdas <apremdas@yahoo.co.uk>
2011-11-24 18:57:49 +0000

    ran "rails g devise User"

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 R3MongoDevise::Application.routes.draw do
+  root :to => 'welcome#index'
+  devise_for :users
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
@@ -46,9 +49,6 @@ R3MongoDevise::Application.routes.draw do
   #     resources :products
   #   end
 
-  # You can have the root of your site routed with "root"
-  # just remember to delete public/index.html.
-  root :to => 'welcome#index'
 
   # See how all your routes lay out with "rake routes"
 
